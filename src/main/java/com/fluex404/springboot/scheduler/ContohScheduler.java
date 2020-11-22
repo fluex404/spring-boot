2ff8bdca19bc1e5a32ea5da7a0f5afd29ab69374
fluex404 <isaabqari@gmail.com>
2020-11-22 22:06:14 +0700

    shedule

---
+++ src/main/java/com/fluex404/springboot/scheduler/ContohScheduler.java
@@ -0,0 +1,17 @@
+package com.fluex404.springboot.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+//@Component
+//public class ContohScheduler {
+//    // cron
+//    @Scheduled(cron = "* * * * * *")
+//    public void cronJobSch() throws Exception {
+//        System.out.println("cron job each 1 minite");
+//    }
+//    @Scheduled(fixedRate = 1000)
+//    public void fixedRateSch() {
+//        System.out.println("fixedrate each 1 minite");
+//    }
+}
