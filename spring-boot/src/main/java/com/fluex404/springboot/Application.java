29e4048ce8cced5ba4c2e1f05019252fe672b60f
fluex404 <isaabqari@gmail.com>
2020-11-22 22:37:32 +0700

    spring boot set eureka client too

--- spring-boot/src/main/java/com/fluex404/springboot/Application.java
+++ spring-boot/src/main/java/com/fluex404/springboot/Application.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class Application {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
