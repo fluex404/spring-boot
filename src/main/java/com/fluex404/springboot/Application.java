350c156f1e6d45336559d10746fbe69cf9b24092
fluex404 <isaabqari@gmail.com>
2020-11-22 19:50:10 +0700

    logging

--- src/main/java/com/fluex404/springboot/Application.java
+++ src/main/java/com/fluex404/springboot/Application.java
@@ -1,13 +1,22 @@
 package com.fluex404.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error");
 	}
 
 }
