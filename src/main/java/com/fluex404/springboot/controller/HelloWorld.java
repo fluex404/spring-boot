f3eef0d53095dc8f18c3a2684a2b485ede45510a
fluex404 <isaabqari@gmail.com>
2020-11-22 18:08:33 +0700

    @Value

--- src/main/java/com/fluex404/springboot/controller/HelloWorld.java
+++ src/main/java/com/fluex404/springboot/controller/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.fluex404.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
@@ -10,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/helloworld")
 public class HelloWorld {
+
+    @Value("${spring.application.name}")
+    public String applicationName;
+
     @GetMapping
     public ResponseEntity<?> getHelloWorld() throws Exception {
         ModelMap m = new ModelMap();
 
+        m.put("application_name", applicationName);
         m.put("name", "Isa Subani");
         m.put("email", "isaabqari@gmail.com");
         m.put("phone", "123123");
